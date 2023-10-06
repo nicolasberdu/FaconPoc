9c4a41f7ec59325b4b79cfd79dd37d2eb33d6cdc
Nicolas <berdunicolas@gmail.com>
2023-10-06 12:44:14 -0300

    Add psr-17 interfaces

---
+++ Facon/Http/Message/RequestFactoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Facon\Http\Message;
+
+use Facon\Http\Message\RequestInterface;
+use Facon\Http\Message\UriInterface;
+
+interface RequestFactoryInterface
+{
+    /**
+     * Create a new request.
+     *
+     * @param string $method The HTTP method associated with the request.
+     * @param UriInterface|string $uri The URI associated with the request. 
+     */
+    public function createRequest(string $method, $uri): RequestInterface;
+}
